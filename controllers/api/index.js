eb224b702ffd0964cccd5ff11c3280a483098770
xndrbrgs <aborgescolon@gmail.com>
2022-03-06 20:52:10 -0500

    api routes covered

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 
 //Require the correct files into the api folder
-
+const userRoute = require('./user-routes')
+const postRoute = require('./post-routes')
+const commentRoute = require('./comment-routes')
 //Create the router.use functionality for each required file
+
+router.use('/users', userRoute);
+router.use('/posts', postRoute);
+router.use('/comments', commentRoute);
 
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -57,6 +57,14 @@ router.post("/login", async (req, res) => {
 
 router.post("/logout", (req, res) => {
   // Create the correct functionality for this post request with error handling
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
