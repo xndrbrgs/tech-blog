50f4ac3c2456f5ccd00d628e99c18c207dace2da
xndrbrgs <aborgescolon@gmail.com>
2022-03-06 20:23:36 -0500

    handling routes first and then models

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
-
 //Require the correct files from the models and authorizations
 
+const Auth = require('../../utils/auth');
+const { Comment } = require('../../models/Comment')
+
 router.post('/', withAuth, async (req, res) => {
-  // Complete the asynchronous function for router.post
+  try {
+    const incomingComment = await Comment.create ({
+      ...req.body, 
+      user_id: req.session.user_id
+    }); 
+
+    res.status(200).json(incomingComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
